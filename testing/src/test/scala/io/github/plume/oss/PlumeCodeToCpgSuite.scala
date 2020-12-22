528ff2a4928ae5f80a29a84547f398209b9497b5
Fabian Yamaguchi <fabs@shiftleft.io>
2020-12-22 15:33:15 +0100

    Fixture + scalafmt

--- testing/src/test/scala/io/github/plume/oss/PlumeCodeToCpgSuite.scala
+++ testing/src/test/scala/io/github/plume/oss/PlumeCodeToCpgSuite.scala
@@ -1,8 +1,8 @@
 package io.github.plume.oss
 import java.io.File
-
-import io.github.plume.oss.drivers.{DriverFactory, GraphDatabase, IDriver, OverflowDbDriver}
+import io.github.plume.oss.drivers.{DriverFactory, GraphDatabase, OverflowDbDriver}
 import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.cpgloading.CpgLoader
 import io.shiftleft.semanticcpg.testfixtures.{CodeToCpgFixture, LanguageFrontend}
 
 import scala.util.Using
@@ -11,12 +11,16 @@ class PlumeFrontend extends LanguageFrontend {
   override val fileSuffix: String = ".java"
 
   override def execute(sourceCodeFile: File): Cpg = {
-
+    val cpgFile = File.createTempFile("plume", ".bin")
+    cpgFile.deleteOnExit()
     Using(DriverFactory.invoke(GraphDatabase.OVERFLOWDB).asInstanceOf[OverflowDbDriver]) { driver =>
-
+      driver.setStorageLocation(cpgFile.getAbsolutePath)
+      val extractor = new Extractor(driver)
+      extractor.load(sourceCodeFile)
+      extractor.project()
+      driver.close()
     }
-
-    ???
+    CpgLoader.load(cpgFile.getAbsolutePath)
   }
 }
 
