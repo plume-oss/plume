d1f3a9c6829c06b60dfea99f29244cfe2a31fe22
Fabian Yamaguchi <fabs@shiftleft.io>
2020-12-22 12:51:48 +0100

    WIP

--- testing/src/test/scala/io/github/plume/oss/PlumeCodeToCpgSuite.scala
+++ testing/src/test/scala/io/github/plume/oss/PlumeCodeToCpgSuite.scala
@@ -1,13 +1,21 @@
 package io.github.plume.oss
 import java.io.File
 
+import io.github.plume.oss.drivers.{DriverFactory, GraphDatabase, IDriver, OverflowDbDriver}
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.semanticcpg.testfixtures.{CodeToCpgFixture, LanguageFrontend}
+
+import scala.util.Using
 
 class PlumeFrontend extends LanguageFrontend {
   override val fileSuffix: String = ".java"
 
   override def execute(sourceCodeFile: File): Cpg = {
+
+    Using(DriverFactory.invoke(GraphDatabase.OVERFLOWDB).asInstanceOf[OverflowDbDriver]) { driver =>
+
+    }
+
     ???
   }
 }
