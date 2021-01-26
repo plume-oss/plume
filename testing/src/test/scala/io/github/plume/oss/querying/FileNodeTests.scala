b08ac3ae1c17f4f5ea85fcb6f83c2176a9ffd6d4
Fabian Yamaguchi <fabs@shiftleft.io>
2021-01-26 13:11:29 +0100

    NamespaceBlock tests (commented out)

--- testing/src/test/scala/io/github/plume/oss/querying/FileNodeTests.scala
+++ testing/src/test/scala/io/github/plume/oss/querying/FileNodeTests.scala
@@ -2,10 +2,9 @@ package io.github.plume.oss.querying
 
 import io.github.plume.oss.PlumeCodeToCpgSuite
 import io.shiftleft.semanticcpg.language._
+import io.shiftleft.semanticcpg.language.types.structure.File
 
 class FileNodeTests extends PlumeCodeToCpgSuite {
-
-  private val UNKNOWN = "<unknown>"
 
   override val code: String =
     """
@@ -13,13 +12,13 @@ class FileNodeTests extends PlumeCodeToCpgSuite {
       |""".stripMargin
 
   "should contain two file nodes in total, both with order=0" in {
-    // cpg.file.order.l shouldBe List(0, 0)
-    // cpg.file.name(UNKNOWN).size shouldBe 1
-    // cpg.file.nameNot().size shouldBe 1
+//     cpg.file.order.l shouldBe List(0, 0)
+//     cpg.file.name(File.UNKNOWN).size shouldBe 1
+//     cpg.file.nameNot().size shouldBe 1
   }
 
   "should contain exactly one placeholder file node with `name=\"<unknown>\"/order=0`" in {
-//    cpg.file(UNKNOWN).l match {
+//    cpg.file(File.UNKNOWN).l match {
 //      case List(x) =>
 //        x.order shouldBe 0
 //      case _ => fail()
@@ -27,7 +26,7 @@ class FileNodeTests extends PlumeCodeToCpgSuite {
   }
 
   "should contain exactly one non-placeholder file with absolute path in `name`" in {
-//    cpg.file.nameNot(UNKNOWN).l match {
+//    cpg.file.nameNot(File.UNKNOWN).l match {
 //      case List(x) =>
 //        x.name should startWith("/")
 //      case _ => fail()
