5f2b5d68cf43a7e8d4ba7072fce395282b2bd987
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-02-15 11:29:31 +0200

    Have tested DiffGraph operations

--- plume/src/main/kotlin/io/github/plume/oss/util/DiffGraphUtil.kt
+++ plume/src/main/kotlin/io/github/plume/oss/util/DiffGraphUtil.kt
@@ -2,19 +2,47 @@ package io.github.plume.oss.util
 
 import io.github.plume.oss.domain.mappers.VertexMapper
 import io.github.plume.oss.drivers.IDriver
+import io.shiftleft.codepropertygraph.generated.nodes.NewNode
 import io.shiftleft.passes.DiffGraph
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import overflowdb.Node
 
+/**
+ * Utility class to process [DiffGraph] changes.
+ */
 object DiffGraphUtil {
 
+    private val logger: Logger = LogManager.getLogger(DiffGraphUtil::class.java)
+
+    /**
+     * Processes [DiffGraph] operations and applies them to the given [IDriver].
+     *
+     * @param driver The driver to which the changes are to be applied.
+     * @param df The [DiffGraph] from which to extract the changes from.
+     */
     fun processDiffGraph(driver: IDriver, df: DiffGraph) {
         df.iterator().foreach { change: DiffGraph.Change ->
-            println(change.javaClass)
             when (change) {
                 is io.shiftleft.passes.`DiffGraph$Change$CreateNode` ->
                     driver.addVertex(VertexMapper.mapToVertex(change.node()))
                 is io.shiftleft.passes.`DiffGraph$Change$RemoveNode` ->
                     driver.deleteVertex(change.nodeId())
-                else -> Unit
+                is io.shiftleft.passes.`DiffGraph$Change$RemoveEdge` -> {
+                    val edge = change.edge()
+                    driver.deleteEdge(
+                        src = VertexMapper.mapToVertex(edge.outNode() as Node),
+                        tgt = VertexMapper.mapToVertex(edge.inNode() as Node),
+                        edge = edge.label()
+                    )
+                }
+                is io.shiftleft.passes.`DiffGraph$Change$CreateEdge` ->
+                    driver.addEdge(
+                        src = VertexMapper.mapToVertex(change.src() as NewNode),
+                        tgt = VertexMapper.mapToVertex(change.dst() as NewNode),
+                        edge = change.label()
+                    )
+                else -> logger.warn("Unsupported DiffGraph operation ${change.javaClass} encountered.")
             }
         }
     }
--- plume/src/test/kotlin/io/github/plume/oss/util/DiffGraphUtilTest.kt
+++ plume/src/test/kotlin/io/github/plume/oss/util/DiffGraphUtilTest.kt
@@ -1,26 +1,38 @@
 package io.github.plume.oss.util
 
-import io.github.plume.oss.TestDomainResources.Companion.INT_1
+import io.github.plume.oss.TestDomainResources
 import io.github.plume.oss.TestDomainResources.Companion.STRING_1
-import io.github.plume.oss.TestDomainResources.Companion.STRING_2
+import io.github.plume.oss.TestDomainResources.Companion.fileVertex
+import io.github.plume.oss.TestDomainResources.Companion.methodVertex
 import io.github.plume.oss.drivers.DriverFactory
 import io.github.plume.oss.drivers.GraphDatabase
 import io.github.plume.oss.drivers.TinkerGraphDriver
+import io.shiftleft.codepropertygraph.generated.EdgeTypes.SOURCE_FILE
 import io.shiftleft.codepropertygraph.generated.NodeKeyNames.NAME
 import io.shiftleft.codepropertygraph.generated.nodes.File
-import io.shiftleft.codepropertygraph.generated.nodes.NewFileBuilder
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import scala.Option
+import overflowdb.Edge
+import scala.Tuple2
+import scala.collection.immutable.Seq
+import scala.jdk.CollectionConverters
 
 class DiffGraphUtilTest {
 
     val driver = (DriverFactory(GraphDatabase.TINKER_GRAPH) as TinkerGraphDriver).apply { connect() }
 
+    @AfterEach
+    fun tearDown() {
+        TestDomainResources.simpleCpgVertices.forEach { it.id(-1) }
+        driver.clearGraph()
+    }
+
     @Test
     fun addNodeTest() {
         val builder = io.shiftleft.passes.DiffGraph.newBuilder()
-        builder.addNode(NewFileBuilder().name(STRING_1).order(INT_1).hash(Option.apply(STRING_2)).build())
+        builder.addNode(fileVertex.build())
 
         DiffGraphUtil.processDiffGraph(driver, builder.build())
         driver.getWholeGraph().use { g ->
@@ -30,18 +42,50 @@ class DiffGraphUtilTest {
 
     @Test
     fun removeNodeTest() {
-        val f= NewFileBuilder().name(STRING_1).order(INT_1).hash(Option.apply(STRING_2))
-        driver.addVertex(f)
+        driver.addVertex(fileVertex)
         driver.getWholeGraph().use { g ->
             assertTrue(g.nodes().asSequence().any { it.label() == File.Label() && it.property(NAME) == STRING_1 })
         }
         val builder = io.shiftleft.passes.DiffGraph.newBuilder()
-        builder.removeNode(f.id())
+        builder.removeNode(fileVertex.id())
 
         DiffGraphUtil.processDiffGraph(driver, builder.build())
         driver.getWholeGraph().use { g ->
             assertTrue(g.nodes().asSequence().none { it.label() == File.Label() && it.property(NAME) == STRING_1 })
         }
+    }
+
+    @Test
+    fun createEdgeTest() {
+        driver.getWholeGraph().use { g -> assertFalse(g.edges().hasNext()) }
+        val builder = io.shiftleft.passes.DiffGraph.newBuilder()
+
+        builder.addEdge(
+            methodVertex.build(),
+            fileVertex.build(),
+            SOURCE_FILE,
+            Seq.from(CollectionConverters.IterableHasAsScala(emptyList<Tuple2<String, Any>>()).asScala())
+        )
+        DiffGraphUtil.processDiffGraph(driver, builder.build())
+        driver.getWholeGraph().use { g ->
+            assertTrue(g.edges().hasNext())
+            val edge = g.edges().next()
+            assertTrue(edge.outNode().label() == methodVertex.build().label())
+            assertTrue(edge.inNode().label() == fileVertex.build().label())
+            assertTrue(edge.label() == SOURCE_FILE)
+        }
+    }
+
+    @Test
+    fun removeEdgeTest() {
+        driver.addEdge(methodVertex, fileVertex, SOURCE_FILE)
+        val edgeToRemove: Edge
+        driver.getWholeGraph().use { g -> edgeToRemove = g.edges().next() }
+        val builder = io.shiftleft.passes.DiffGraph.newBuilder()
+        builder.removeEdge(edgeToRemove)
+
+        DiffGraphUtil.processDiffGraph(driver, builder.build())
+        driver.getWholeGraph().use { g -> assertFalse(g.edges().hasNext()) }
     }
 
 }
