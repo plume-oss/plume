b8c010b4d5df5c0465aa0ae9983558c7ef4a699c
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-03-04 16:30:36 +0200

    Added timer

---
+++ plume/src/main/kotlin/io/github/plume/oss/metrics/ExtractorTimer.kt
@@ -0,0 +1,47 @@
+package io.github.plume.oss.metrics
+
+object ExtractorTimer {
+
+    private val totalTimes = mutableMapOf(
+        ExtractorTimeKey.LOADING_AND_COMPILING to 0L,
+        ExtractorTimeKey.UNIT_GRAPH_BUILDING to 0L,
+        ExtractorTimeKey.DATABASE_WRITE to 0L,
+        ExtractorTimeKey.DATABASE_READ to 0L,
+        ExtractorTimeKey.CPG_PASSES to 0L
+    )
+
+    private val stopwatch = mutableMapOf(
+        ExtractorTimeKey.LOADING_AND_COMPILING to System.nanoTime(),
+        ExtractorTimeKey.UNIT_GRAPH_BUILDING to System.nanoTime(),
+        ExtractorTimeKey.DATABASE_WRITE to System.nanoTime(),
+        ExtractorTimeKey.DATABASE_READ to System.nanoTime(),
+        ExtractorTimeKey.CPG_PASSES to System.nanoTime()
+    )
+
+    fun startTimerOn(key: ExtractorTimeKey) {
+        stopwatch[key] = System.nanoTime()
+    }
+
+    fun stopTimerOn(key: ExtractorTimeKey) {
+        totalTimes.computeIfPresent(key) { u, t -> t + (System.nanoTime() - stopwatch.getOrDefault(u, System.nanoTime())) }
+    }
+
+    fun stopAll() {
+        ExtractorTimeKey.values().forEach(this::stopTimerOn)
+    }
+
+    fun reset() {
+        ExtractorTimeKey.values().forEach { totalTimes[it] = 0L }
+    }
+
+    fun getTimes(): Map<ExtractorTimeKey, Long> = totalTimes.toMap()
+
+    enum class ExtractorTimeKey {
+        LOADING_AND_COMPILING,
+        UNIT_GRAPH_BUILDING,
+        DATABASE_WRITE,
+        DATABASE_READ,
+        CPG_PASSES
+    }
+}
+
