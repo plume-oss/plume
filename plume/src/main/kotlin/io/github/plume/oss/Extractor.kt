1e7cd6df73dd3eb894ac3d5148c18a0be96b6ddd
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-03-15 12:02:35 +0200

    :zap: Parallel Base CPG Building (#106)
    * Added delta graph and have setup infrastructure for local graph building
    
    * Split jobs into threads for concurrency advantage
    
    * Added delta graph tests
    
    * Have all minus 1 test working
    
    * Have all minus 1 test working
    
    * Parallelized method body building
    
    * Documented new changes
    
    * Cleaned up base pass
    
    * Fixed line number issue
    
    * Reduced chunk size to have writes start faster
    
    * Optimize changes in the delta graph

--- plume/src/main/kotlin/io/github/plume/oss/Extractor.kt
+++ plume/src/main/kotlin/io/github/plume/oss/Extractor.kt
@@ -20,6 +20,7 @@ import io.github.plume.oss.domain.files.FileFactory
 import io.github.plume.oss.domain.files.JVMClassFile
 import io.github.plume.oss.domain.files.PlumeFile
 import io.github.plume.oss.domain.files.UnsupportedFile
+import io.github.plume.oss.domain.model.DeltaGraph
 import io.github.plume.oss.drivers.GremlinDriver
 import io.github.plume.oss.drivers.IDriver
 import io.github.plume.oss.drivers.Neo4jDriver
@@ -28,10 +29,7 @@ import io.github.plume.oss.metrics.ExtractorTimeKey
 import io.github.plume.oss.metrics.PlumeTimer
 import io.github.plume.oss.options.ExtractorOptions
 import io.github.plume.oss.passes.SCPGPass
-import io.github.plume.oss.passes.graph.ASTPass
-import io.github.plume.oss.passes.graph.CFGPass
-import io.github.plume.oss.passes.graph.CGPass
-import io.github.plume.oss.passes.graph.PDGPass
+import io.github.plume.oss.passes.graph.*
 import io.github.plume.oss.passes.method.MethodStubPass
 import io.github.plume.oss.passes.structure.ExternalTypePass
 import io.github.plume.oss.passes.structure.FileAndPackagePass
@@ -48,6 +46,9 @@ import io.shiftleft.codepropertygraph.generated.NodeKeyNames.*
 import io.shiftleft.codepropertygraph.generated.NodeTypes.META_DATA
 import io.shiftleft.codepropertygraph.generated.NodeTypes.METHOD
 import io.shiftleft.codepropertygraph.generated.nodes.NewMetaDataBuilder
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import soot.*
@@ -60,6 +61,7 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.concurrent.Executors
 import java.util.stream.Collectors
 import java.util.zip.ZipFile
 import kotlin.streams.asSequence
@@ -233,7 +235,7 @@ class Extractor(val driver: IDriver) {
         PlumeTimer.measure(ExtractorTimeKey.BASE_CPG_BUILDING) {
             val allMs: List<SootMethod> = (parentToChildCs.flatMap { it.second + it.first }.flatMap { it.methods }
                     + sootUnitGraphs.map { it.body.method }).distinct().toList()
-            val existingMs: List<String> = driver.getPropertyFromVertices(FULL_NAME, METHOD)
+            val existingMs = driver.getPropertyFromVertices<String>(FULL_NAME, METHOD).toSet()
             // Create method stubs while avoiding duplication
             logger.info("Building stubs for all methods")
             pipeline(
@@ -244,17 +246,34 @@ class Extractor(val driver: IDriver) {
             })
             // Create method bodies while avoiding duplication
             logger.info("Building method bodies for all internal methods")
-            pipeline(
-                // Base CPG
-                ASTPass(driver)::runPass,
-                CFGPass(driver)::runPass,
-                PDGPass(driver)::runPass,
-                // Call graph
-                CGPass(driver)::runPass,
-            ).invoke(sootUnitGraphs.filterNot { sm ->
+            val bodiesToBuild = sootUnitGraphs.filterNot { sm ->
                 val (fullName, _, _) = SootToPlumeUtil.methodToStrings(sm.body.method)
                 existingMs.contains(fullName)
-            })
+            }.toList()
+            runBlocking {
+                val chunkSize = 25
+                val jobCount = bodiesToBuild.size
+                val nThreads = (jobCount / chunkSize)
+                    .coerceAtLeast(1)
+                    .coerceAtMost(Runtime.getRuntime().availableProcessors())
+                val channel = Channel<DeltaGraph>()
+                logger.debug("Running $jobCount jobs over $nThreads thread(s).")
+                // Create jobs in chunks and submit these jobs to a thread pool
+                val threadPool = Executors.newFixedThreadPool(nThreads)
+                launch {
+                    bodiesToBuild.chunked(chunkSize)
+                        .map { chunk -> Runnable { buildBaseCPGs(chunk, channel) } }
+                        .forEach(threadPool::submit)
+                }
+                // Consumer: Receive delta graphs and write changes to the driver in serial
+                launch {
+                    repeat(jobCount) { channel.receive().apply(driver) }
+                    logger.debug("All $jobCount jobs have been applied to the driver")
+                    channel.close()
+                }.join() // Suspend until the channel is fully consumed.
+            }
+            // Connect call edges
+            CGPass(driver).runPass(bodiesToBuild)
         }
         // Clear all Soot resources and cache
         clear()
@@ -268,6 +287,16 @@ class Extractor(val driver: IDriver) {
 
     private fun <T> pipeline(vararg functions: (T) -> T): (T) -> T =
         { functions.fold(it) { output, stage -> stage.invoke(output) } }
+
+    /**
+     * Constructs the method bodies concurrently.
+     *
+     * @param gs The list of method bodies as [BriefUnitGraph]s.
+     */
+    private fun buildBaseCPGs(gs: List<BriefUnitGraph>, channel: Channel<DeltaGraph>) = runBlocking {
+        // Producer: Create local instances of CPGs from BriefUnitGraphs map changes to DeltaGraphs
+        gs.forEach { g -> launch { channel.send(BaseCPGPass(g).runPass()) } }
+    }
 
     /**
      * Load all methods to construct the CPG from and convert them to [BriefUnitGraph] objects.
---
+++ plume/src/main/kotlin/io/github/plume/oss/domain/model/DeltaGraph.kt
@@ -0,0 +1,79 @@
+package io.github.plume.oss.domain.model
+
+import io.github.plume.oss.drivers.IDriver
+import io.shiftleft.codepropertygraph.generated.nodes.NewNodeBuilder
+
+/**
+ * This is based off of [io.shiftleft.passes.DiffGraph]. Where DiffGraphs do not propagate ID changes, this one does by
+ * making use of [NewNodeBuilder] objects. These get assigned an ID when passed to [IDriver.addVertex] so no duplication
+ * occurs.
+ */
+class DeltaGraph private constructor(private val changes: List<Delta>) {
+
+    private constructor(builder: Builder) : this(builder.getChanges())
+
+    /**
+     * Applies stored changes to the given driver.
+     *
+     * @param driver The driver to write changes to.
+     */
+    fun apply(driver: IDriver) {
+        changes.forEach { d ->
+            when (d) {
+                is VertexAdd -> driver.addVertex(d.n)
+                is VertexDelete -> driver.deleteVertex(d.id, d.label)
+                is EdgeAdd -> driver.addEdge(d.src, d.dst, d.e)
+                is EdgeDelete -> driver.deleteEdge(d.src, d.dst, d.e)
+            }
+        }
+    }
+
+    /**
+     * Builds an [DeltaGraph] instance by accumulating changes.
+     */
+    class Builder {
+
+        private val changes = mutableListOf<Delta>()
+
+        /**
+         * Returns a list of the accumulated changes.
+         */
+        fun getChanges() = optimizeChanges()
+
+        /**
+         * Will remove [VertexAdd] deltas if there is already an [EdgeAdd] associated with one of the vertices. This
+         * will cut down on unnecessary database operations.
+         */
+        private fun optimizeChanges(): List<Delta> {
+            val optimizedChanges = mutableListOf<Delta>()
+            val vertexAdds = changes.filterIsInstance<VertexAdd>().toMutableList()
+            changes.filterIsInstance<EdgeAdd>().forEach { ea ->
+                vertexAdds.firstOrNull { it.n == ea.src }?.let { va -> vertexAdds.remove(va) }
+                vertexAdds.firstOrNull { it.n == ea.dst }?.let { va -> vertexAdds.remove(va) }
+            }
+            changes.filterNot { it is VertexAdd }.toCollection(optimizedChanges)
+            vertexAdds.toCollection(optimizedChanges)
+            return optimizedChanges.toList()
+        }
+
+        fun addVertex(n: NewNodeBuilder) = apply { changes.add(VertexAdd(n)) }
+
+        fun deleteVertex(n: NewNodeBuilder) = apply { changes.add(VertexDelete(n.id(), n.build().label())) }
+
+        fun deleteVertex(id: Long, label: String) = apply { changes.add(VertexDelete(id, label)) }
+
+        fun addEdge(src: NewNodeBuilder, tgt: NewNodeBuilder, e: String) = apply { changes.add(EdgeAdd(src, tgt, e)) }
+
+        fun deleteEdge(src: NewNodeBuilder, tgt: NewNodeBuilder, e: String) =
+            apply { changes.add(EdgeDelete(src, tgt, e)) }
+
+        fun build() = DeltaGraph(this)
+    }
+
+    abstract class Delta
+    data class VertexAdd(val n: NewNodeBuilder) : Delta()
+    data class VertexDelete(val id: Long, val label: String) : Delta()
+    data class EdgeAdd(val src: NewNodeBuilder, val dst: NewNodeBuilder, val e: String) : Delta()
+    data class EdgeDelete(val src: NewNodeBuilder, val dst: NewNodeBuilder, val e: String) : Delta()
+
+}
