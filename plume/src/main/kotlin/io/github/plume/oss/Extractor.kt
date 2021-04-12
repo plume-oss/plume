920c49c9f4e09ac82a93749cd23a5efa79e68887
JIN-HYUK JUNG <nemojjh@gmail.com>
2021-04-12 17:45:00 +0900

    Fix for Native Method  - Project Parameters of Native Method with Bytecode

--- plume/src/main/kotlin/io/github/plume/oss/Extractor.kt
+++ plume/src/main/kotlin/io/github/plume/oss/Extractor.kt
@@ -327,7 +327,7 @@ class Extractor(val driver: IDriver) {
             Method body level analysis - only done on new/updated methods
          */
         logger.info("Running data flow passes")
-//        PlumeTimer.measure(ExtractorTimeKey.DATA_FLOW_PASS) { DataFlowPass(driver).runPass() }
+        PlumeTimer.measure(ExtractorTimeKey.DATA_FLOW_PASS) { DataFlowPass(driver).runPass() }
         PlumeStorage.methodCpgs.clear()
         return this
     }
@@ -339,6 +339,7 @@ class Extractor(val driver: IDriver) {
 
     private fun getMethodsAndBuildBodies(cs: Set<SootClass>): Set<SootMethod> {
         return cs.flatMap { it.methods }
+            .filter { runCatching { it.retrieveActiveBody()}.getOrNull() != null || it.isNative }
             .map { m ->
                 val ms = mutableSetOf(m)
                 if (ExtractorOptions.callGraphAlg != ExtractorOptions.CallGraphAlg.NONE) {
