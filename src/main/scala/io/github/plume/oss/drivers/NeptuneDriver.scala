509c321c70a8e71a8743555b856e931f5efa677e
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-11-24 11:18:39 +0200

    Have bulk reset code ready

--- src/main/scala/io/github/plume/oss/drivers/NeptuneDriver.scala
+++ src/main/scala/io/github/plume/oss/drivers/NeptuneDriver.scala
@@ -1,17 +1,18 @@
 package io.github.plume.oss.drivers
 
+import io.circe.Encoder
+import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
 import io.github.plume.oss.drivers.NeptuneDriver.DEFAULT_PORT
 import io.shiftleft.codepropertygraph.generated.nodes.{AbstractNode, NewNode, StoredNode}
 import io.shiftleft.passes.AppliedDiffGraph
-import org.apache.commons.configuration.BaseConfiguration
 import org.apache.tinkerpop.gremlin.driver.Cluster
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection
-import org.apache.tinkerpop.gremlin.process.traversal.{AnonymousTraversalSource, P, Traverser}
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.{GraphTraversalSource, __}
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.has
-import org.apache.tinkerpop.gremlin.structure.{Graph, T}
+import org.apache.tinkerpop.gremlin.process.traversal.{AnonymousTraversalSource, P}
 import org.slf4j.{Logger, LoggerFactory}
-import sttp.client3.{Empty, RequestT, basicRequest}
+import sttp.client3.circe._
+import sttp.client3.{Empty, HttpURLConnectionBackend, Identity, RequestT, SttpBackend, basicRequest}
+import sttp.model.Uri
 
 import scala.jdk.CollectionConverters.IteratorHasAsScala
 import scala.util.Using
@@ -23,6 +24,9 @@ class NeptuneDriver(
 ) extends GremlinDriver {
 
   override protected val logger: Logger = LoggerFactory.getLogger(classOf[NeptuneDriver])
+
+  implicit val payloadEncoder: Encoder[InitiateResetBody] =
+    Encoder.forProduct1("action")(u => u.action)
 
   private val cluster = Cluster
     .build()
@@ -38,7 +42,8 @@ class NeptuneDriver(
   override def isConnected: Boolean = !cluster.isClosed
 
   override def clear(): Unit = {
-    val noVs = Using.resource(traversal()) { g => g.V().count().next() }
+    val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
+    val noVs                                = Using.resource(traversal()) { g => g.V().count().next() }
     if (noVs < 10000) {
       Using.resource(traversal()) { g =>
         var deleted = 0L
@@ -49,9 +54,47 @@ class NeptuneDriver(
         }
       }
     } else {
-      // TODO: Do HTTP bulk delete
+      val systemUri = Uri("https", hostname, port).addPath(Seq("system"))
+      val token: String = request()
+        .post(systemUri)
+        .body(InitiateResetBody())
+        .response(asJson[InitiateResetResponse])
+        .send(backend)
+        .body match {
+        case Left(e)              => logger.error("Unable to initiate database reset!"); throw e
+        case Right(resetResponse) => resetResponse.token
+      }
+      request()
+        .post(systemUri)
+        .body(PerformResetBody(token))
+        .send(backend)
+        .body match {
+        case Left(e) =>
+          logger.error("Unable to perform database reset!")
+          throw new RuntimeException(e)
+        case Right(_) =>
+          val statusUri = Uri("https", hostname, port).addPath(Seq("status"))
+          Iterator
+            .continually(
+              request()
+                .get(statusUri)
+                .response(asJson[InstanceStatusResponse])
+                .send(backend)
+                .body
+            )
+            .takeWhile {
+              case Left(e)         => logger.warn("Unable to obtain instance status", e); true
+              case Right(response) => if (response.status == "healthy") true else false
+            }
+            .foreach(_ => Thread.sleep(5000))
+      }
     }
   }
+
+  case class InitiateResetBody(action: String = "initiateDatabaseReset")
+  case class InitiateResetResponse(token: String)
+  case class PerformResetBody(token: String, action: String = "performDatabaseReset")
+  case class InstanceStatusResponse(status: String)
 
   override def id(node: AbstractNode, dg: AppliedDiffGraph): Any =
     node match {
